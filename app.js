18c0ee7ba051b86dff93b0b2c7c5f616a4744a20
subhash1717 <yadavsubhash1717@gmail.com>
2026-07-14 13:54:03 +0530

    some change i app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Google DNS
+
 if(process.env.NODE_ENV != "production") {
     require('dotenv').config();
 }
@@ -10,6 +13,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const { MongoStore } = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -21,7 +25,8 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js")
 
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wonderlust';
+//const MONGO_URL = 'mongodb://127.0.0.1:27017/wonderlust';
+const dbUrl = process.env.ATLASDB_URL;
 
 
 main().then(() =>{
@@ -31,7 +36,7 @@ main().then(() =>{
 });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -40,8 +45,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error",() =>{
+    console.log("ERROR in mongo store",err)
+});
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -51,9 +69,11 @@ const sessionOptions = {
     },
 };
 
-app.get("/",(req, res) =>{
-    res.send("HI, i am root");
-});
+// app.get("/",(req, res) =>{
+//     res.send("HI, i am root");
+// });
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
